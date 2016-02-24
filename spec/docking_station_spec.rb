f86780056b036b3fa4a186beef6b7953d4660371
JoeWroe <joe_wroe@icloud.com>
2016-02-24 11:24:05 +0000

    USER STORY ONE
    Added test for releasing bike.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,12 @@ require 'docking_station'
 
 describe DockingStation do
 
+  describe 'when releasing bikes' do
+
+    it 'responds to #release_bike' do
+      expect(subject).to respond_to :release_bike
+    end
+
+  end
+
 end
