08134a91a51f2c6ba0003ebbc4b013c31499b428
JoeWroe <joe_wroe@icloud.com>
2016-02-25 11:14:21 +0000

    REPORTING BROKEN
    Added unit test for reporting a bike as broken.

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,6 +4,14 @@ describe Bike do
 
   subject(:bike) {described_class.new}
 
+  describe 'when a user can reports a bike as broken' do
+
+    it 'is not working' do
+      bike.report_broken
+      expect(bike.working).to eq(false)
+    end
+  end
+
   describe 'so a user can get a working bike' do
 
     it 'responds to #working?' do
